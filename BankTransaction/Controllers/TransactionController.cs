4dcbe86cfe1894cfb812668af9600e62168f7b38
elmin <elmin_oman@hotmail.com>
2023-07-16 23:40:23 +0200

    Initial commit

---
+++ BankTransaction/Controllers/TransactionController.cs
@@ -0,0 +1,78 @@
+﻿using BankTransaction.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BankTransaction.Controllers
+{
+    public class TransactionController : Controller
+    {
+        private readonly TransactionDbContext _context;
+
+        public TransactionController(TransactionDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Transaction
+        public async Task<IActionResult> Index()
+        {
+              return _context.Transactions != null ? 
+                          View(await _context.Transactions.ToListAsync()) :
+                          Problem("Entity set 'TransactionDbContext.Transactions'  is null.");
+        }
+
+
+        // GET: Transaction/AddorEdit
+        public IActionResult AddorEdit(int id=0)
+        {
+            if(id == 0)
+            return View(new Transaction());
+            else
+                return View(_context.Transactions.Find(id));
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> AddorEdit([Bind("TransactionId,AccountNumber,BeneficiaryName,BankName,SWIFTCode,Amount,Date")] Transaction transaction)
+        {
+            if (ModelState.IsValid)
+            {
+                if (transaction.TransactionId == 0)
+                {
+                    transaction.Date = DateTime.Now;
+                    _context.Add(transaction);
+                }
+                else
+                _context.Update(transaction);
+
+                    
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(transaction);
+        }
+
+       
+
+
+        [HttpPost]
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Transactions == null)
+            {
+                return Problem("Entity set 'TransactionDbContext.Transactions'  is null.");
+            }
+            var transaction = await _context.Transactions.FindAsync(id);
+            if (transaction != null)
+            {
+                _context.Transactions.Remove(transaction);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
